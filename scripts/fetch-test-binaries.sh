175f07583dd79291c57ac3f061f0522be40e7cd3
vadim <vadim@estcard.ee>
2020-04-30 14:56:00 +0300

    publish 1.0.0-RELEASE

--- scripts/fetch-test-binaries.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl https://storage.googleapis.com/kubebuilder-tools/kubebuilder-tools-1.15.5-linux-amd64.tar.gz -o kubebuilder-tools.tar.gz
-tar -zvxf kubebuilder-tools.tar.gz
-
-rm kubebuilder-tools.tar.gz
